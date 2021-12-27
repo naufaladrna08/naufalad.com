fe122dba9e4ef3e5cc594a54da011a1631ff8515
naufaladrna08 <naufaladrn90@gmail.com>
2021-12-27 20:39:22 +0700

    added users model and migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ class CreateUsersTable extends Migration {
       $table->string('username')->unique();
       $table->string('name');
       $table->string('email')->unique();
+      $table->integer('photo_id');
       $table->timestamp('email_verified_at')->nullable();
       $table->string('password');
       $table->rememberToken();
