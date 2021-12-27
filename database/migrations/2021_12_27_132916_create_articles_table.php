269397ca72ce2516f5018181e5497e56465114ca
naufaladrna08 <naufaladrn90@gmail.com>
2021-12-27 20:37:01 +0700

    added model and migration for articles

---
+++ database/migrations/2021_12_27_132916_create_articles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration {
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up() {
+    Schema::create('articles', function (Blueprint $table) {
+      $table->id();
+      $table->integer('uid');
+      $table->string('title');
+      $table->string('content');
+      $table->string('categories');
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down() {
+    Schema::dropIfExists('articles');
+  }
+
+  public function uid() {
+    return $this->belongsTo(User::class, 'foreign_key', 'owner_key');
+  }
+}
